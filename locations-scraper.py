79d9b3f203812e5173de1a9135c9cb4c91b0d597
candacej97 <ccj286@nyu.edu>
2021-12-12 02:36:54 -0500

    - retrieve name and address of all locations - place data into json object

--- locations-scraper.py
+++ locations-scraper.py
@@ -9,6 +9,7 @@ Start at dollartree.com/locations
 
 import requests
 from bs4 import BeautifulSoup
+import json
 
 URL = "https://dollartree.com/locations"
 page = requests.get(URL)
@@ -17,11 +18,13 @@ page = requests.get(URL)
 # print(page.text.encode('utf-8'))
 
 soup = BeautifulSoup(page.content, "html.parser")
+data = {}
 
 # Find, loop through, and print each state (class=ga_w2gi_lp) 
 states = soup.find_all("a", class_="ga_w2gi_lp")
 for state in states:
     # print(state, end="\n"*2)
+    data[state] = []
     
     # Get link to state page
     pageOfCities = requests.get(state["href"])
@@ -30,13 +33,27 @@ for state in states:
     # Find, loop through, and print each city (class=ga_w2gi_lp) 
     cities = citySoup.find_all("a", class_="ga_w2gi_lp")
     for city in cities:
-        print(city, end="\n"*2)
+        # print(city, end="\n"*2)
 
         # Get link to city page
         pageOfStores = requests.get(city["href"])
         storesSoup = BeautifulSoup(pageOfStores.content, "html.parser")
 
-        # Find, loop through, and print each store (id="listings")
-        stores = pageOfStores.find_all("a", id="listings")
+        # Find, loop through, and print each store (class="schemastore")
+        stores = storesSoup.find_all("div", class_="item_div")
         for store in stores:
-            print(store, end="\n"*2)
+            # print(store, end="\n"*2)
+
+            # Get name and address of location
+            print(store.contents[1].text)
+            # print(store.contents[4])
+            print("{}, {}, {} {}".format(store.contents[4].contents[1].text, store.contents[4].contents[4].text, store.contents[4].contents[6].text, store.contents[4].contents[8].text))
+
+            name = store.contents[1].text
+            address = "{}, {}, {} {}".format(store.contents[4].contents[1].text, store.contents[4].contents[4].text, store.contents[4].contents[6].text, store.contents[4].contents[8].text)
+
+            data[state].append({
+                "name": name,
+                "address": address,
+                "base": 1.00
+            })
