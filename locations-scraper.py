219fea9602feb24fc897298f8d9a7f3726d54eeb
candacej97 <ccj286@nyu.edu>
2021-12-12 00:17:51 -0500

    complete scraping trail

--- locations-scraper.py
+++ locations-scraper.py
@@ -1,12 +1,10 @@
 # Scrape US locations only
-# Check on the 1st of every month for new locations
-# 
+
 '''
 Start at dollartree.com/locations
     Loop through each state (class=ga_w2gi_lp)
         Loop through each city (class=ga_w2gi_lp)
             Loop through each store (id="listings")
-                Get information as csv? (Store #, Name, Address, Hours, Phone Number)
 ''' 
 
 import requests
@@ -23,5 +21,22 @@ soup = BeautifulSoup(page.content, "html.parser")
 # Find, loop through, and print each state (class=ga_w2gi_lp) 
 states = soup.find_all("a", class_="ga_w2gi_lp")
 for state in states:
-    print(state, end="\n"*2)
+    # print(state, end="\n"*2)
+    
+    # Get link to state page
+    pageOfCities = requests.get(state["href"])
+    citySoup = BeautifulSoup(pageOfCities.content, "html.parser")
 
+    # Find, loop through, and print each city (class=ga_w2gi_lp) 
+    cities = citySoup.find_all("a", class_="ga_w2gi_lp")
+    for city in cities:
+        print(city, end="\n"*2)
+
+        # Get link to city page
+        pageOfStores = requests.get(city["href"])
+        storesSoup = BeautifulSoup(pageOfStores.content, "html.parser")
+
+        # Find, loop through, and print each store (id="listings")
+        stores = pageOfStores.find_all("a", id="listings")
+        for store in stores:
+            print(store, end="\n"*2)
